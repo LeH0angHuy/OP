3a1b1b9e0c25efb1097f740d8d5fddcd9ccaae11
LeH0angHuy <52832937+LeH0angHuy@users.noreply.github.com>
2024-03-04 19:22:36 +0700

    Update CKT-lag.lua

--- CKT-lag.lua
+++ CKT-lag.lua
@@ -1,4 +1,3 @@
---[[
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-188.7037353515625, 8.777145385742188, 78.1675033569336)
 wait(0.75)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-187.8138885498047, 8.777145385742188, 67.15576171875)
